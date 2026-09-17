c6f48902781c7f21d0734355a7be962aedd6f45c
Oyeng <Arroyan23@users.noreply.github.com>
2026-09-18 03:25:20 +0800

    fix: configure HME for Vercel serverless deployment

---
+++ api/index.js
@@ -0,0 +1,24 @@
+import { app } from '../server/app.js';
+import { migrate } from '../server/migrate.js';
+
+let migrationPromise;
+
+export default async function handler(req, res) {
+  migrationPromise ??= migrate().catch((error) => {
+    migrationPromise = undefined;
+    throw error;
+  });
+  await migrationPromise;
+
+  await new Promise((resolve, reject) => {
+    res.once('finish', resolve);
+    res.once('close', resolve);
+    try {
+      app(req, res);
+    } catch (error) {
+      reject(error);
+    }
+  });
+}
+
+export { app };
